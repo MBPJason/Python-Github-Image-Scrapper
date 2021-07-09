4004208cf95d8d8cec2b57ee2335c190a2883a0c
Jason Ozulumba <cjozulumba@gmail.com>
2021-07-09 17:23:18 -0400

    Built user image web scrapper for github users

---
+++ README.md
@@ -0,0 +1,3 @@
+# Basic introduction
+
+This is a simple web scrapper designed to pull inputted users pictures from github.
---
+++ main.py
@@ -0,0 +1,9 @@
+import requests as request
+from bs4 import BeautifulSoup as bs
+
+github_user = input('Input GitHub User: ')
+url = 'https://github.com/' + github_user
+r = request.get(url)
+soup = bs(r.content, 'html.parser')
+profile_image = soup.find('img', {'alt': 'Avatar'})['src']
+print(profile_image)
